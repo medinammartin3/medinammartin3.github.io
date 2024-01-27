0b4e1430126855308cd93fa9bbfa0ea362f6b137
Martin <medinammartin3@gmail.com>
2024-01-27 16:57:37 -0500

    nav dynamic background

--- static/script.js
+++ static/script.js
@@ -1,15 +1,99 @@
 // Sticky nav bar
-window.onscroll = function() {stick()};
+window.onscroll = function() {
+  stick();
+  changeNavBackground();
+};
 
-var header = document.getElementById("nav");
-
-var sticky = header.offsetTop;
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
 
 function stick() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+  if (window.scrollY > sticky + 15 ) {
+    nav.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    nav.classList.remove("sticky");
+  }
+}
+
+// Nav actual section background change
+
+// About
+var about = document.getElementById("about");
+var aboutNav = document.getElementById("aboutNav");
+var aboutTop = about.offsetTop;
+var aboutBottom = aboutTop + about.offsetHeight;
+
+// Education
+var education = document.getElementById("education")
+var educationNav = document.getElementById("educationNav");
+var educationTop = education.offsetTop;
+var educationBottom = educationTop + education.offsetHeight;
+
+// Experience
+var experience = document.getElementById("experience")
+var experienceNav = document.getElementById("experienceNav");
+var experienceTop = experience.offsetTop;
+var experienceBottom = experienceTop + experience.offsetHeight;
+
+// Programming
+var programming = document.getElementById("programming")
+var programmingNav = document.getElementById("programmingNav");
+var programmingTop = programming.offsetTop;
+var programmingBottom = programmingTop + programming.offsetHeight;
+
+// Projects
+var projects = document.getElementById("projects")
+var projectsNav = document.getElementById("projectsNav");
+var projectsTop = projects.offsetTop;
+var projectsBottom = projectsTop + projects.offsetHeight;
+
+// Contact
+var contact = document.getElementById("contact")
+var contactNav = document.getElementById("contactNav");
+var contactTop = contact.offsetTop;
+var contactBottom = contactTop + contact.offsetHeight;
+
+function changeNavBackground() {
+  // About
+  if ((window.scrollY > aboutTop - 170) && (window.scrollY < aboutBottom - 170)) {
+    aboutNav.classList.add("backgroundChange");
+  } else {
+    aboutNav.classList.remove("backgroundChange");
+  }
+
+  // Education
+  if ((window.scrollY > educationTop - 170) && (window.scrollY < educationBottom - 170)) {
+    educationNav.classList.add("backgroundChange");
+  } else {
+    educationNav.classList.remove("backgroundChange");
+  }
+
+  // Experience
+  if ((window.scrollY > experienceTop - 170) && (window.scrollY < experienceBottom - 170)) {
+    experienceNav.classList.add("backgroundChange");
+  } else {
+    experienceNav.classList.remove("backgroundChange");
+  }
+
+  // Programming
+  if ((window.scrollY > programmingTop - 170) && (window.scrollY < programmingBottom - 170)) {
+    programmingNav.classList.add("backgroundChange");
+  } else {
+    programmingNav.classList.remove("backgroundChange");
+  }
+
+  // Projects
+  if ((window.scrollY > projectsTop - 170) && (window.scrollY < projectsBottom - 170)) {
+    projectsNav.classList.add("backgroundChange");
+  } else {
+    projectsNav.classList.remove("backgroundChange");
+  }
+
+  // Contact
+  if ((window.scrollY > contactTop - 170) && (window.scrollY < contactBottom - 170)) {
+    contactNav.classList.add("backgroundChange");
+  } else {
+    contactNav.classList.remove("backgroundChange");
   }
 }
 
