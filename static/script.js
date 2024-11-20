e8acb6e99fbc278ab9708c33c885a71d90d14faa
Martin <medinammartin3@gmail.com>
2024-11-19 21:04:54 -0500

    Modify website content focus

--- static/script.js
+++ static/script.js
@@ -23,6 +23,12 @@ var aboutNav = document.getElementById("aboutNav");
 var aboutTop = about.offsetTop;
 var aboutBottom = aboutTop + about.offsetHeight;
 
+// Projects
+var projects = document.getElementById("projects")
+var projectsNav = document.getElementById("projectsNav");
+var projectsTop = projects.offsetTop;
+var projectsBottom = projectsTop + projects.offsetHeight;
+
 // Education
 var education = document.getElementById("education")
 var educationNav = document.getElementById("educationNav");
@@ -35,24 +41,6 @@ var experienceNav = document.getElementById("experienceNav");
 var experienceTop = experience.offsetTop;
 var experienceBottom = experienceTop + experience.offsetHeight;
 
-// Programming
-var programming = document.getElementById("programming");
-var programmingNav = document.getElementById("programmingNav");
-var programmingTop = programming.offsetTop;
-var programmingBottom = programmingTop + programming.offsetHeight;
-
-// Projects
-var projects = document.getElementById("projects")
-var projectsNav = document.getElementById("projectsNav");
-var projectsTop = projects.offsetTop;
-var projectsBottom = projectsTop + projects.offsetHeight;
-
-// Contact
-var contact = document.getElementById("contact")
-var contactNav = document.getElementById("contactNav");
-var contactTop = contact.offsetTop;
-var contactBottom = contactTop + contact.offsetHeight;
-
 function changeNavBackground() {
   // About
   if ((window.scrollY > aboutTop - 170) && (window.scrollY < aboutBottom - 170)) {
@@ -60,6 +48,13 @@ function changeNavBackground() {
   } else {
     aboutNav.classList.remove("backgroundChange");
   }
+
+    // Projects
+    if ((window.scrollY > projectsTop - 170) && (window.scrollY < projectsBottom - 170)) {
+      projectsNav.classList.add("backgroundChange");
+    } else {
+      projectsNav.classList.remove("backgroundChange");
+    }
 
   // Education
   if ((window.scrollY > educationTop - 170) && (window.scrollY < educationBottom - 170)) {
@@ -73,29 +68,6 @@ function changeNavBackground() {
     experienceNav.classList.add("backgroundChange");
   } else {
     experienceNav.classList.remove("backgroundChange");
-  }
-
-  // Programming
-  if ((window.scrollY > programmingTop - 170) && (window.scrollY < programmingBottom - 170)) {
-    programmingNav.classList.add("backgroundChange");
-    // Activate animations
-    $(".progressbar").addClass('progressBarsAnimation'); 
-  } else {
-    programmingNav.classList.remove("backgroundChange");
-  }
-
-  // Projects
-  if ((window.scrollY > projectsTop - 170) && (window.scrollY < projectsBottom - 170)) {
-    projectsNav.classList.add("backgroundChange");
-  } else {
-    projectsNav.classList.remove("backgroundChange");
-  }
-
-  // Contact
-  if ((window.scrollY > contactTop - 170) && (window.scrollY < contactBottom - 170)) {
-    contactNav.classList.add("backgroundChange");
-  } else {
-    contactNav.classList.remove("backgroundChange");
   }
 }
 
